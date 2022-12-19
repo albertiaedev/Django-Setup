d6053c3366dc7fa160f8636813192b041c77e7c8
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-12-19 19:06:31 -0400

    apps.py
    It is a common configuration file for all Django apps. You can configure the attributes for your app using this file. However, the default configuration is sufficient for most cases. So, adding app configuration is a rare case.

--- Django-Setup/App/apps.py
+++ Django-Setup/App/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'App'
+
+    
+'''
+Django-Setup
+by J.A. Hernández
+'''
